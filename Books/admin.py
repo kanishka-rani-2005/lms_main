2a2775a4a1bedf1dc2158855c2b2d5d3619ed329
Kanishka Rani <kanishka22043@gmail.com>
2025-07-31 16:35:51 +0530

    Register Models

--- Books/admin.py
+++ Books/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from Books.models import Book
+# Register your models here.
+
+
+admin.site.register(Book)
